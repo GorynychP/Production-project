953acfebbe22e2d1d1d05332287268c6d3b75ce2
Ivan <ivananatolevic66@gmail.com>
2023-12-16 15:02:11 +0300

    add checks CI

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,7 +7,7 @@ export function buildPlugins({
 	paths,
 	isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-	return [
+	const plugins = [
 		new HtmlWebpackPlugin({
 			template: paths.html,
 		}),
@@ -19,7 +19,11 @@ export function buildPlugins({
 		new webpack.DefinePlugin({
 			__IS_DEV__: JSON.stringify(isDev),
 		}),
-		new webpack.HotModuleReplacementPlugin(),
-		new BundleAnalyzerPlugin({ openAnalyzer: false }),
 	];
+	if (isDev) {
+		plugins.push(new webpack.HotModuleReplacementPlugin());
+		plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+	}
+
+	return plugins;
 }
